44422e5b59b14dc05c989b7d264f518d8340c46b
ArtemPurtoian <artemios.od@gmail.com>
2024-01-25 21:28:54 +0200

    added fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,3 +6,17 @@ from app import app
 def client():
     with app.test_client() as client:
         yield client
+
+
+@pytest.fixture
+def get_welcome_url(client):
+    url = "api/welcome"
+    response = client.get(url)
+    return response
+
+
+@pytest.fixture
+def create_user(client, post_body):
+    url = "api/users"
+    response = client.post(url, json=post_body)
+    return response
