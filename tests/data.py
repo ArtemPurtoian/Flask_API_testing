e7f51284d9771fbe7c920f93b6acfe55ae199c2c
ArtemPurtoian <artemios.od@gmail.com>
2026-06-10 11:22:44 +0300

    Moved test data to ext module

---
+++ tests/data.py
@@ -0,0 +1,54 @@
+VALID_USER = {
+    "user_name": "alex",
+    "gender": "male",
+    "age": 30
+}
+
+UPDATED_USER = {
+    "user_name": "john",
+    "gender": "male",
+    "age": 25
+}
+
+SECOND_USER = {
+    "user_name": "bob",
+    "gender": "male",
+    "age": 20
+}
+
+
+INVALID_USERNAME_TYPE = {
+    "user_name": 1,
+    "gender": "male",
+    "age": 30
+}
+
+INVALID_GENDER_TYPE = {
+    "user_name": "alex",
+    "gender": 1,
+    "age": 30
+}
+
+INVALID_AGE_TYPE = {
+    "user_name": "alex",
+    "gender": "male",
+    "age": "30"
+}
+
+
+MISSING_USERNAME = {
+    "gender": "male",
+    "age": 30
+}
+
+MISSING_GENDER = {
+    "user_name": "alex",
+    "age": 30
+}
+
+MISSING_AGE = {
+    "user_name": "alex",
+    "gender": "male"
+}
+
+EMPTY_PAYLOAD = {}
